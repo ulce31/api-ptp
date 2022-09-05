eb77de64f2e03e536e2c9a733ac24538597f74ab
ULCE <ulcechrist.pro@gmail.com>
2022-09-05 15:49:33 +0100

    Api successfully connected the database

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+async function connectToDB() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USR_DB}:${process.env.PWD_DB}@${process.env.ADR_DB}/?retryWrites=true&w=majority`
+    );
+    console.log("Succesfully connected to the DB ⚡️");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.get("/", async (req, res) => {
   res.send("TEST route");
 });
-
 app.listen(port, () => {
   console.log(`PTP API listens on ${port} port.`);
+  connectToDB();
 });
