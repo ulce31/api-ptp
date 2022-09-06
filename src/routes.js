e21680586bfb76efbd0e309196abe1640c7a5047
ULCE <ulcechrist.pro@gmail.com>
2022-09-06 10:12:08 +0100

    MVP version of register API route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import userController from "./Controllers/userController.js";
 
 const router = Router();
 
 router.get("/", function testRoute(req, res) {
   res.send("TEST route");
 });
+
+router.post("/register", userController.register);
 
 export default router;
