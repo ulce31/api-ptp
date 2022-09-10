355806bac1788f4706c297acc7cfcd1b842e0e77
ULCE <ulcechrist.pro@gmail.com>
2022-09-10 17:23:24 +0100

    Session management at the tail of

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
+import supertokens from "supertokens-node";
+import Session from "supertokens-node/recipe/session/index.js";
+
 import mongoose from "mongoose";
 const app = express();
 const port = 3000;
 
 import router from "./src/routes.js";
+import {
+  errorHandler,
+  middleware,
+} from "supertokens-node/lib/build/framework/express/framework.js";
+
+supertokens.init({
+  framework: "express",
+  supertokens: {
+    connectionURI: process.env.ST_URI,
+    apiKey: process.env.ST_API_KEY,
+  },
+  appInfo: {
+    appName: "ptp",
+    apiDomain: "http://localhost:3000",
+    websiteDomain: "http://localhost:3001",
+    apiBasePath: "/",
+    websiteBasePath: "/",
+  },
+  recipeList: [Session.init()],
+});
 
 async function connectToDB() {
   try {
@@ -28,7 +52,18 @@ async function connectToDB() {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+    credentials: true,
+  })
+);
+app.use(middleware());
+
 app.use("/", router);
+
+app.use(errorHandler());
 
 app.listen(port, () => {
   console.log(`PTP API listens on ${port} port.`);
--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,5 +1,6 @@
 import userModel from "../Models/userModel.js";
 import crypto from "crypto";
+import Session from "supertokens-node/recipe/session/index.js";
 
 const register = async (req, res) => {
   try {
@@ -26,12 +27,20 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   try {
     let { email, password } = req.body;
-    const userInDB = await userModel.findOne({
-      email,
-      password: crypto.createHash("sha256").update(password).digest("hex"),
-    });
+    const userInDB = await userModel
+      .findOne({
+        email,
+        password: crypto.createHash("sha256").update(password).digest("hex"),
+      })
+      .select("-password");
     if (userInDB) {
-      console.log("logged");
+      //AIM: Store User's role
+      await Session.createNewSession(res, userInDB.id);
+      /* a new session has been created.
+       * - an access & refresh token has been attached to the response's cookie
+       * - a new row has been inserted into the database for this new session
+       */
+      res.status(200).send("Successfully logged ✅");
     }
   } catch (error) {
     console.log(error);
